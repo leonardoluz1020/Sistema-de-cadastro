39e2fd2c6f94b7297105eb39b367cfb5827a3b95
Leonardo Luz <leonardoluz10@hotmail.com>
2022-09-26 11:17:50 -0300

    Voltando para o estado do colab

--- app.js
+++ app.js
@@ -2,6 +2,5 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando com o banco de dados")
 // mais um console log de teste :
 console.log("O colaborador fez esse commit sem voce ver na aula!")
-console.log("Enviando para o GitHub")
 
 
