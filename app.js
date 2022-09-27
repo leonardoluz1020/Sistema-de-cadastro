8acb9b1c71597e483070dea60c9bf3222a4db831
Leonardo Luz <leonardoluz10@hotmail.com>
2022-09-27 14:41:39 -0300

    Corrigindo excesso de linhas

--- app.js
+++ app.js
@@ -2,5 +2,3 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando com o banco de dados")
 // mais um console log de teste :
 console.log("O colaborador fez esse commit sem voce ver na aula!")
-
-
